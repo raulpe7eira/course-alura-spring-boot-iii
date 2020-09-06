de7ee7ea8ce5448e525dd3ee853d6908281fc783
Raul Pereira <raul.pereira@gmail.com>
2020-09-05 22:55:17 -0300

    class 05 - docker

--- forum-monitor/src/test/java/br/com/alura/monitoramento/MonitoramentoApplicationTests.java
+++ forum-monitor/src/test/java/br/com/alura/monitoramento/MonitoramentoApplicationTests.java
@@ -2,15 +2,13 @@ package br.com.alura.monitoramento;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class MonitoramentoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
